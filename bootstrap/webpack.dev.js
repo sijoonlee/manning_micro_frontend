0df7265b96068939748c0a5661123bd007d887bb
sijoonlee <shijoonlee@gmail.com>
2021-07-24 13:55:25 -0400

    1st step

---
+++ bootstrap/webpack.dev.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: './src/index.html'
+        }),
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/',
+    },
+};
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+
+const app = express();
+
+app.use("/mfe/music", express.static("./music/build"));
+app.use("/mfe/welcome", express.static("./welcome/dist"));
+app.use("/", express.static("./bootstrap/dist"));
+
+app.listen(3001, () => {
+    console.log("Web Server running at: http://localhost:3001")
+    console.log("http://localhost:3001/hello")
+    console.log("http://localhost:3001/play")
+});
